15238d5fb9a3402736ec672e2c3142a6d76693ab
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-05 16:18:58 +0100

    print "Hello, World!" to the console n times using a while loop

--- Hello Worlds/main.cpp
+++ Hello Worlds/main.cpp
@@ -2,9 +2,15 @@
 
 int main() {
     unsigned int n{};
+    unsigned int whileLoopCounter{};
+
     std::cin >> n;
 
     for (int i = 0; i < n; i++) {
+        std::cout << "Hello, World!" << std::endl;
+    }
+
+    while (whileLoopCounter++ < n) {
         std::cout << "Hello, World!" << std::endl;
     }
 

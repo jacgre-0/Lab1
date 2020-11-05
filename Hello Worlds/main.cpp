7d6a5aadc73af9fc63236f8c60127c3442a8a955
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-05 16:31:36 +0100

    Added additional prints used for padding so it is easier to see which loop did the printing in the console window.

--- Hello Worlds/main.cpp
+++ Hello Worlds/main.cpp
@@ -7,14 +7,17 @@ int main() {
 
     std::cin >> n;
 
+    std::cout << "Using a for loop:" << std::endl;
     for (int i = 0; i < n; i++) {
         std::cout << "Hello, World!" << std::endl;
     }
 
+    std::cout << "\nUsing a while loop:" << std::endl;
     while (whileLoopCounter++ < n) {
         std::cout << "Hello, World!" << std::endl;
     }
 
+    std::cout << "\nUsing a do while loop:" << std::endl;
     do {
         std::cout << "Hello, World!" << std::endl;
     } while (++doWhileLoopCounter < n);

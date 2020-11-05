11f6b5961e52fba907bce44c682cdfed64002723
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-05 22:11:59 +0100

    Added code for the bounce function

--- Bounce/main.cpp
+++ Bounce/main.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+
+void Bounce(int n);
+
 int main() {
+    int n{};
+    std::cout << "Give a natural number: ";
+    std::cin >> n;
+    Bounce(n);
     return 0;
+}
+
+void Bounce(int n) {
+    if (n < 0) {
+        std::cout << "Error! Input cannot be negative." << std::endl;
+        return;
+    }
+
+    for (int i = n; i > 0; i--) {
+        std::cout << i;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        std::cout << i;
+    }
 }
